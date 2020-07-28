02eb9b06241c61cc44ef2e9e8a0d4b5241df94bd
Jonatan <jrperdomoz@gmail.com>
2020-07-28 21:48:53 +0300

    no hay necesidad de pasar store a los handlers

--- src/pages/login/handlers.ts
+++ src/pages/login/handlers.ts
@@ -2,9 +2,9 @@ import { Player } from '@runox-game/game-engine/lib/models/player.model';
 import type { IPlayer } from '@runox-game/game-engine/lib/models/player.model';
 import firebase from "../../services/firebase-app";
 import { AppStatus, IStore } from "../../store/types";
-import { store as _store } from '../../store';
+import { store } from '../../store';
 
-export const login = async (store: IStore) => {
+export const login = async () => {
   const provider = new firebase.auth.GoogleAuthProvider();
   try {
     const result = await firebase.auth().signInWithPopup(provider);
@@ -21,16 +21,16 @@ export const login = async (store: IStore) => {
 
 };
 
-export const handleRoomName = (store: IStore, roomName: string) => {
+export const handleRoomName = (roomName: string) => {
   store.setRoomName(roomName)
 }
 
 export const handleFixRoom = (key: string) => {
   if (key === 'Enter') {
-    _store.setHasRoomNameFixed(true);
+    store.setHasRoomNameFixed(true);
   }
 }
 
 export const logout = (store: IStore) => {
-  _store.reset();
+  store.reset();
 }
