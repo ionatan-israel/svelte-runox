a59c3e47324d583ff9f58e21066a615d07fe9eb9
Jonatan <jrperdomoz@gmail.com>
2020-07-28 21:38:40 +0300

    handleFixRoom and logout hanlders (comunicacion con store)

--- src/pages/login/handlers.ts
+++ src/pages/login/handlers.ts
@@ -2,6 +2,7 @@ import { Player } from '@runox-game/game-engine/lib/models/player.model';
 import type { IPlayer } from '@runox-game/game-engine/lib/models/player.model';
 import firebase from "../../services/firebase-app";
 import { AppStatus, IStore } from "../../store/types";
+import { store as _store } from '../../store';
 
 export const login = async (store: IStore) => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -22,4 +23,14 @@ export const login = async (store: IStore) => {
 
 export const handleRoomName = (store: IStore, roomName: string) => {
   store.setRoomName(roomName)
+}
+
+export const handleFixRoom = (key: string) => {
+  if (key === 'Enter') {
+    _store.setHasRoomNameFixed(true);
+  }
+}
+
+export const logout = (store: IStore) => {
+  _store.reset();
 }
